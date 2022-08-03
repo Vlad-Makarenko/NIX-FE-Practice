df14e5499982ad62f20b027966c68ab80ce93cae
Vlad-Makarenko <Makrenkovladok@gmail.com>
2022-08-03 16:25:51 +0300

    isEven func added

---
+++ Task3/src/isEven.js
@@ -0,0 +1,7 @@
+
+const isEven = num => !(num & 1);   
+
+console.log(isEven(2));      //true
+console.log(isEven(3));      //false
+console.log(isEven(10));     //true
+console.log(isEven(15));     //false
