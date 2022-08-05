0e16cf4b0d2050a39f2f1e0aaafc840d7e22313e
Vlad-Makarenko <Makrenkovladok@gmail.com>
2022-08-05 15:58:31 +0300

    func renamed circleArray => circledArray

---
+++ Task3/src/circledArray.js
@@ -0,0 +1,27 @@
+class circledArray {
+  constructor() {
+    this.array = [];
+  }
+
+  add(value) {
+    this.array.push(value);
+  }
+
+  remove(index) {
+    if (index >= 0) {
+      this.array.splice(index % this.array.length, 1);
+    } else {
+      index = -index % this.array.length;
+      this.array.splice(this.array.length - index, 1);
+    }
+  }
+
+  get(index) {
+    if (index >= 0) {
+      return this.array[index % this.array.length];
+    } else {
+      index = -index % this.array.length;
+      return this.array[this.array.length - index];
+    }
+  }
+}
